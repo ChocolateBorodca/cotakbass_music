cbaeb5e411d421fbac87ddf41fc5613f8be93fd4
ChocolateBorodca <ashatkhabibullin118@gmail.com>
2026-05-20 02:09:43 +0300

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,12 +1,13 @@
 import { generateClientTokenFromReadWriteToken } from '@vercel/blob';
 
 export default async function handler(req, res) {
+    // Разрешаем только POST запросы
     if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
 
     try {
         const { fileName } = req.body;
 
-        // Создаем одноразовый клиентский токен для безопасной загрузки напрямую с телефона
+        // Создаем защищенный одноразовый токен для загрузки напрямую
         const clientToken = await generateClientTokenFromReadWriteToken({
             token: process.env.BLOB_READ_WRITE_TOKEN,
             username: 'cotakbass_user',
