3d7f0bea55ae0f33ed44334296702ec9e029dfd9
ChocolateBorodca <ashatkhabibullin118@gmail.com>
2026-05-20 01:29:02 +0300

    Create list.js

---
+++ api/list.js
@@ -0,0 +1,10 @@
+import { list } from '@vercel/blob';
+
+export default async function handler(req, res) {
+    try {
+        const { blobs } = await list({ prefix: 'music/' });
+        return res.status(200).json({ blobs });
+    } catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
+}
